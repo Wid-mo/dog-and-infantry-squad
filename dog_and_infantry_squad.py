b6fa6d0cfc17988e67725df8f385cee29e47cae0
Bartosz Panek <bartekxpanek@wp.pl>
2022-11-17 09:38:36 +0100

    Revert "Finish introduction section"
    This reverts commit b4f3a04963b688054cc80b3b3e77cc8c51c24cbc.

--- dog_and_infantry_squad.py
+++ dog_and_infantry_squad.py
@@ -4,7 +4,6 @@ from manim import *
 class DogAndInfantrySquad(Scene):
     def construct(self):
         """INTRODUCTION"""
-        self.next_section("Introduction", skip_animations=True)
 
         infantry_squad = Square(
             side_length=3, color=GREEN, fill_opacity=0.3, stroke_color=GREEN
@@ -62,20 +61,6 @@ class DogAndInfantrySquad(Scene):
         g = VGroup(question_text, line1, line2, arrow).set_color(RED)
         self.play(FadeOut(dog), FadeIn(g))
         self.wait()
-
-        """CUES 1/3"""
-        self.next_section("cues 1/3")
-
-        dot = Dot([-2, -1, 0])
-        dot2 = Dot([2, 1, 0])
-        line = Line(dot.get_center(), dot2.get_center()).set_color(ORANGE)
-        b1 = Brace(line)
-        b1text = b1.get_text("Horizontal distance")
-        b2 = Brace(line, direction=line.copy().rotate(PI / 2).get_unit_vector())
-        b2text = b2.get_tex("x-x_1")
-        self.add(line, dot, dot2, b1, b2, b1text, b2text)
-
-
 
 
 # manim -p -ql dog_and_infantry_squad.py DogAndInfantrySquad
